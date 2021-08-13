d090682e76d7120e621cbab22aa8edb7ab6e688e
petrn <petrn@users.noreply.github.com>
2021-08-13 16:50:35 +0200

    Update zvbi.rb

--- Formula/zvbi.rb
+++ Formula/zvbi.rb
@@ -3,13 +3,13 @@ class Zvbi < Formula
   homepage 'http://zapping.sourceforge.net/'
   url "http://downloads.sourceforge.net/project/zapping/zvbi/0.2.35/zvbi-0.2.35.tar.bz2"
   sha256 "fc883c34111a487c4a783f91b1b2bb5610d8d8e58dcba80c7ab31e67e4765318"
+  version "0.2.35-pn" # to distinguish from homebrew-core's ffmpeg
+  revision 1
 
     # two of the unit tests fail on Mac OS - see
   # http://sourceforge.net/p/zapping/bugs/197/ - so remove them until
   # fixed so that we can run the rest of the tests
-  def patches
-    DATA
-  end
+  patch :DATA
 
   def install
     system "./configure", "--disable-dependency-tracking",
