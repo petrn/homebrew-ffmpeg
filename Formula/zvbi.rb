d19ae6f880316daee1330ec4823e0d2e16a0b831
petrn <petrn@users.noreply.github.com>
2021-08-13 16:42:23 +0200

    Update zvbi.rb

--- Formula/zvbi.rb
+++ Formula/zvbi.rb
@@ -4,9 +4,7 @@ class Zvbi < Formula
   url "http://downloads.sourceforge.net/project/zapping/zvbi/0.2.35/zvbi-0.2.35.tar.bz2"
   sha256 "fc883c34111a487c4a783f91b1b2bb5610d8d8e58dcba80c7ab31e67e4765318"
 
-  depends_on "pkg_config"
-  
-  # two of the unit tests fail on Mac OS - see
+    # two of the unit tests fail on Mac OS - see
   # http://sourceforge.net/p/zapping/bugs/197/ - so remove them until
   # fixed so that we can run the rest of the tests
   def patches
