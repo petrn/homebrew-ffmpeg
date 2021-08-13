96ae092b4c926aa0cedf9611a89b57c1013f01c5
petrn <petrn@users.noreply.github.com>
2021-08-13 16:52:19 +0200

    Update zvbi.rb

--- Formula/zvbi.rb
+++ Formula/zvbi.rb
@@ -4,7 +4,7 @@ class Zvbi < Formula
   url "http://downloads.sourceforge.net/project/zapping/zvbi/0.2.35/zvbi-0.2.35.tar.bz2"
   sha256 "fc883c34111a487c4a783f91b1b2bb5610d8d8e58dcba80c7ab31e67e4765318"
   version "0.2.35-pn" # to distinguish from homebrew-core's ffmpeg
-  revision 1
+  revision 2
 
     # two of the unit tests fail on Mac OS - see
   # http://sourceforge.net/p/zapping/bugs/197/ - so remove them until
